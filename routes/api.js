40e078bf7e7dd988d096982a45f967893d2fce68
andrei101010 <andrei101010@outlook.com>
2023-02-19 18:16:34 +0900

    backend & frontend test

--- routes/api.js
+++ routes/api.js
@@ -19,13 +19,18 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 /* Upload a video */
-router.post('/upload', upload.single('video'), function (req, res, next) {
+router.post('/upload', () => {
+  if (!fs.existsSync("public/videos")) {
+    fs.mkdirSync("public/videos");
+  }
+}, upload.single('video'), function (req, res, next) {
   let fname = "public/videos/" + req.file.filename;
   let dname = "public/frames/" + req.file.filename;
+  console.log(fname, " ", dname);
   if (!fs.existsSync(dname)) {
     fs.mkdirSync(dname);
   }
-  execSync("ffmpeg -i " + fname + " -vf fps=5 " + dname + "/" + "%07d.png");
+  execSync("ffmpeg -i " + fname + " -vf fps=1 " + dname + "/" + "%07d.png");
   return res.send({
     statusOK: true,
     filename: req.file.filename
@@ -37,7 +42,7 @@ const timeInSeconds = (str) => {
   let result = 0;
   for (let x of arr) {
     result *= 60;
-    result += x;
+    result += parseInt(x);
   }
   return result;
 }
@@ -81,21 +86,14 @@ router.post('/save/:fname', async function (req, res, next) {
       if (fs.existsSync("emptyFrom.mp4")) {
         fs.unlinkSync("emptyFrom.mp4");
       }
-      // console.log("asdfasdf");
       if (fs.existsSync("emptyTo.mp4")) {
         fs.unlinkSync("emptyTo.mp4");
       }
-      // console.log("asdfasdfasdfasdf");
-      // if (fs.existsSync("concatenationList.txt")) {
-      //   fs.unlinkSync("concatenationList.txt");
-      // }
       if (fs.existsSync("output.mp4")) {
         fs.unlinkSync("output.mp4");
       }
-      // console.log("asdfasdfasdfasdfasdfasdf");
       let front_length = timeInSeconds(new_start);
       let back_length = parseInt(length) - timeInSeconds(new_end);
-      // console.log(length, back_length);
       let concatList = [];
       if (front_length > 0) {
         execSync(`ffmpeg -t ${timeInSeconds(new_start)} -f lavfi -i color=c=black:s=${width}x${height} -c:v libx264 -tune stillimage -pix_fmt yuv420p emptyFrom.mp4`);
@@ -118,47 +116,26 @@ router.post('/save/:fname', async function (req, res, next) {
         concatList.push("emptyTo.mp4");
         fs.unlinkSync('output.mp4');
       }
-      // var mergedVideo = ffmpeg();
-      // concatList.forEach(function(videoName){
-      //   mergedVideo = mergedVideo.addInput(videoName);
-      // });
-      // await(new Promise((resolve, reject) => {
-      //   mergedVideo.mergeToFile("output.mp4").on('error', function(error) {
-      //     reject(error);
-      //   }).on('end', function() {
-      //     resolve();
-      //   })
-      // }));
       let commandString = "ffmpeg ";
       for (let filename of concatList) {
         commandString += ` -i ${filename} `;
       }
-      // console.log("OKAY");
       commandString += '-filter_complex "';
       width = 1280, height = 720;
       for (let index in concatList) {
-        // console.log(index, "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf");
         commandString += `[${index}:v]scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:-1:-1,setsar=1,fps=30,format=yuv420p[v${index}];`;
       }
       for (let index in concatList) {
-        // console.log(index, "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf");
         commandString += `[${index}:a]aformat=sample_rates=48000:channel_layouts=stereo[a${index}];`;
       }
       for (let index in concatList) {
         commandString += `[v${index}][a${index}]`;
       }
       commandString += `concat=n=${concatList.length}:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -c:v libx264 -c:a aac -movflags +faststart output.mp4`;
-      // console.log(commandString);
-      // console.log(front_length, medium_length, back_length);
-      // fs.writeFileSync("concatenationList.txt", concatenation_string);
-      // console.log("asdfasdfasdfasdfasdfasdfasdfasdf");
-      // console.log("start here");
       execSync(commandString);
-      // console.log("end here");
       fs.unlinkSync(newName);
       fs.copyFileSync("output.mp4", newName);
       fs.unlinkSync("output.mp4");
-      // fs.unlinkSync("concatenationList.txt");
       fs.unlinkSync("emptyFrom.mp4");
       fs.unlinkSync("emptyTo.mp4");
     } catch (error) {
